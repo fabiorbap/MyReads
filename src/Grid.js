ef8c686108c0a6315ce652c3433a3fe04be87d4a
fabiorbap <fabiorbap@gmail.com>
2018-03-24 17:25:50 -0300

    Test API call with grid generator

--- src/Grid.js
+++ src/Grid.js
@@ -1,7 +1,14 @@
-mapToBookItems = (books) => {
-    return books.map(book => {
-        <BookItem backgroundImage={book.imageLinks.thumbnail}
-            title={book.title}
-        authors={book.authors}/>
-    })
+import React from 'react'
+import { BookItem } from './BookItem';
+
+export const mapToBookItems = (books) => {
+    return books.map((book, key) => {
+        return(
+            <li key={key}>
+                <BookItem backgroundImage={book.imageLinks.thumbnail}
+                    bookTitle={book.title}
+                    bookAuthors={book.authors} />
+            </li>
+        );
+    });
 }
