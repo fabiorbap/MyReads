e66b62ee17e2b3cf7921e9bdf43ce8ad86bc3f3f
fabiorbap <fabiorbap@gmail.com>
2018-03-25 17:51:09 -0300

    feature: Sort books by section

--- src/BookMapper.js
+++ src/BookMapper.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BookItem } from './BookItem';
 
 export const mapToBookItems = (books) => {
+    console.log(books);
     return books.map((book, key) => {
         return(
             <li key={key}>
